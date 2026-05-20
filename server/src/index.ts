870108930ff05c4ca353d37dd6e1f82df332bca9
rbantug <ronbantug22@gmail.com>
2026-05-20 12:10:59 +0800

    feat: implement express server with CORS and status endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: 'ok'
+    })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`backend is running on port ${PORT}`)
+})
