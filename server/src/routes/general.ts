a59944cbd3405ec163c9a7233bf0ed3f75ad930b
rbantug <ronbantug22@gmail.com>
2026-05-25 00:02:50 +0800

    feat: refactor general routes to use controller functions for health and embedding test endpoints

--- server/src/routes/general.ts
+++ server/src/routes/general.ts
@@ -1,29 +1,11 @@
 import { Router } from "express";
-import axios from "axios";
+
+import { getHealth, getEmbeddingTest } from "../controllers/generalController.js";
 
 const router = Router();
 
-router.get("/health", (_, res) => {
-  res.json({
-    status: "ok",
-  });
-});
+router.get("/health", getHealth);
 
-router.get("/embedding-test", async (_, res) => {
-  const model = process.env.OLLAMA_MODEL;
-  async function getData() {
-    try {
-      const response = await axios.post("http://ollama:11434/api/embeddings", {
-        model,
-        prompt: "healthy filipino soup",
-      });
-
-      res.json(response.data);
-    } catch (error:any) {
-        console.error(error.message)
-    }
-  }
-  await getData()
-});
+router.get("/embedding-test", getEmbeddingTest);
 
 export default router;
