a92d2e27276fbb993f88fb61431e1dc3fe9e976c
rbantug <ronbantug22@gmail.com>
2026-05-27 17:07:26 +0800

    feat: implement CRUD operations for recipes with validation and indexing

--- server/src/controllers/recipeController.ts
+++ server/src/controllers/recipeController.ts
@@ -6,6 +6,9 @@ import type { Request, Response, NextFunction } from "express";
 
 import catchAsyncError from "../../utils/catchAsyncError.js";
 import AppError from "../../utils/appError.js";
+import { validateCreateRecipe, validateUpdateRecipe } from "../joiValidation.js";
+import { indexRecipe } from "../search/indexRecipe.js";
+import { deleteRecipeIndex } from "../search/deleteRecipe.js";
 
 export const getAllRecipes = catchAsyncError(
   async (_: Request, res: Response) => {
@@ -20,24 +23,95 @@ export const getAllRecipes = catchAsyncError(
 
 export const getSingleRecipe = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
-    const id  = req.params.id
+    const id = req.params.id;
 
-    if (!id || typeof id === 'object') {
-        return next(new AppError('A recipe id is required', 404))
+    if (!id || typeof id === "object") {
+      return next(new AppError("A recipe id is required", 404));
     }
 
-    const recipe = await db
-      .select()
-      .from(recipes)
-      .where(eq(recipes.id, id));
+    const recipe = await db.select().from(recipes).where(eq(recipes.id, id));
 
     if (!recipe.length) {
-        return next(new AppError('Recipe not found', 404))
+      return next(new AppError("Recipe not found", 404));
     }
 
     res.status(200).json({
       status: "success",
       data: recipe[0],
+    });
+  },
+);
+
+export const postSingleRecipe = catchAsyncError(
+  async (req: Request, res: Response ) => {
+    const validateBody = validateCreateRecipe(req.body)
+
+    const insertedId = await db
+      .insert(recipes)
+      .values(validateBody)
+      .returning({ insertedId: recipes.id });
+
+    await indexRecipe(validateBody);
+
+    res.status(200).json({
+      status: "success",
+      data: insertedId,
+    });
+  },
+);
+
+export const updateSingleRecipe = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+
+    const id = req.params.id;
+
+    if (!id || typeof id === "object") {
+      return next(new AppError("A recipe id is required", 404));
+    }
+
+    const recipe = await db.select().from(recipes).where(eq(recipes.id, id));
+
+    if (!recipe.length) {
+      return next(new AppError("Recipe not found", 404));
+    }
+
+    const validateBody = validateUpdateRecipe({ ...recipe[0], ...req.body });
+
+    const updatedRecipe = {
+      ...validateBody,
+      createdAt: recipe[0]?.createdAt,
+      updatedAt: new Date(),
+    };
+
+    const result = await db
+      .update(recipes)
+      .set(updatedRecipe)
+      .where(eq(recipes.id, id));
+
+    await indexRecipe(updatedRecipe);
+
+    res.status(200).json({
+      status: "success",
+      result,
+    });
+  },
+);
+
+export const deleteSingleRecipe = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    if (!id || typeof id === "object") {
+      return next(new AppError("A recipe id is required", 404));
+    }
+
+    const result = await db.delete(recipes).where(eq(recipes.id, id));
+
+    await deleteRecipeIndex(id)
+
+    res.status(200).json({
+      status: "success",
+      result,
     });
   },
 );
