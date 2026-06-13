8a757473e18e84b1eaf5aee4739f1f640a126a56
rbantug <ronbantug22@gmail.com>
2026-06-13 17:09:07 +0000

    feat: added routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import SearchPage from '@/pages/SearchPage.vue'
+import RecipePage from '@/pages/RecipePage.vue'
+import ErrorPage from '@/pages/ErrorPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: SearchPage },
+    { path: '/:id', component: RecipePage },
+    { path: '/:anything(.*)', component: ErrorPage },
+  ],
 })
 
 export default router
