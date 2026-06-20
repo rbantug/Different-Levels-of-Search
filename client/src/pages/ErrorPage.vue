e2f4979566e7ddc4c14031d27b589a29b8aab3a6
rbantug <ronbantug22@gmail.com>
2026-06-20 09:56:45 +0000

    feat: added a error page for vue router

---
+++ client/src/pages/ErrorPage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>Error page. Go away.</div>
+    <router-link to="/">Back to home</router-link>
+</template>
+
+<style lang="scss" scoped></style>
