ee5c8e686bf5b54ea5f707a562ebce7fedffb9c4
rbantug <ronbantug22@gmail.com>
2026-06-20 09:54:46 +0000

    feat: created a component with v-model for selecting keyword or hybrid search

---
+++ client/src/components/SearchPageComponents/DropDown.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const searchOption = defineModel<string>('search-option', { required: true })
+</script>
+
+<template>
+    <div>
+        <select v-model="searchOption">
+            <option value="keyword">Keyword Search only</option>
+            <option value="hybrid">Keyword + Semantic Search</option>
+        </select>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
