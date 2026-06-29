219dd07f9841ee0d14b97d5e0b758e87c9c0aed9
rbantug <ronbantug22@gmail.com>
2026-06-29 09:24:23 +0000

    feat: added a basic badge component

---
+++ client/src/components/util/BaseBadge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="badge-container">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.badge-container {
+    @include m-flex-center;
+    padding: 0.5rem 1rem;
+    background-color: $color-background-badge;
+    color: white;
+    font-weight: 600;
+    border: none;
+    border-radius: 20px;
+}
+</style>
