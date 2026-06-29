5ccf18156ec9e07be406ac4bdc9da6d2b0a95132
rbantug <ronbantug22@gmail.com>
2026-06-29 09:22:44 +0000

    feat: added font style to the entire project

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="main">
     <RouterView />
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main {
+  font-family: $font-family-base;
+}
+</style>
