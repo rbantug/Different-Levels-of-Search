67ab2faede097ae832882c97b161c17b027a840f
rbantug <ronbantug22@gmail.com>
2026-07-02 08:46:37 +0000

    style: the app will not be responsive. It will be permanently in mobile view.

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,12 +2,20 @@
 
 <template>
   <div class="main">
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .main {
   font-family: $font-family-base;
+  max-width: 360px;
+  height: 800px;
+  margin: 0 auto;
+  border: 1px solid black;
 }
 </style>
