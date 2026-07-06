ba3e955c6920c38abd49ed4d2b881d86861abb80
rbantug <ronbantug22@gmail.com>
2026-07-06 03:41:05 +0000

    feat: added a skeleton loading card

--- client/src/components/SearchPageComponents/RecipeCard.vue
+++ client/src/components/SearchPageComponents/RecipeCard.vue
@@ -1,43 +1,68 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+
+import RecipeCardSkeleton from './RecipeCardSkeleton.vue'
 
 const props = defineProps({
   recipeName: {
     required: true,
     type: String,
-    default: ''
+    default: '',
   },
   recipeThumbnail: {
     required: true,
     type: String,
-    default: ''
+    default: '',
   },
   area: {
     required: true,
     type: String,
-    default: ''
+    default: '',
   },
   category: {
     required: true,
     type: String,
-    default: ''
+    default: '',
   },
 })
+
+const imageLoaded = ref(false)
+
+watch(
+  () => props.recipeThumbnail,
+  (newVal) => {
+    if (!newVal) return
+
+    imageLoaded.value = false
+    const img = new Image()
+    img.onload = () => {
+      imageLoaded.value = true
+    }
+
+    img.onerror = () => {
+      imageLoaded.value = true
+    }
+
+    img.src = newVal
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <div class="card-container">
-    <div class="card">
-      <div class="recipe-thumbnail">
-        <img
-          :src="props.recipeThumbnail"
-          :alt="props.recipeName"
-          class="recipe-thumbnail__img"
-        />
-      </div>
-      <div class="recipe-details">
-        <h3 class="recipe-details__name">{{ props.recipeName }}</h3>
-        <p class="recipe-details__area">Cuisine: {{ props.area }}</p>
-        <p class="recipe-details__category">Category: {{ props.category }}</p>
+  <div>
+    <div class="card-container">
+      <RecipeCardSkeleton v-if="!imageLoaded" />
+
+      <div class="card" :style="{ opacity: imageLoaded ? 1 : 0 }">
+        <div class="recipe-thumbnail">
+          <img :src="props.recipeThumbnail" :alt="props.recipeName" class="recipe-thumbnail__img" />
+        </div>
+        <div class="recipe-details">
+          <h3 class="recipe-details__name">{{ props.recipeName }}</h3>
+          <p class="recipe-details__area">Cuisine: {{ props.area }}</p>
+          <p class="recipe-details__category">Category: {{ props.category }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -56,7 +81,9 @@ const props = defineProps({
   outline: 1px solid $color-primary;
   border-radius: 5px;
   background-color: $color-surface;
-  transition: background-color 250ms ease-out, outline 250ms ease-out;
+  transition:
+    background-color 250ms ease-out,
+    outline 250ms ease-out;
 
   @include m-flex-center;
 
@@ -73,22 +100,22 @@ const props = defineProps({
 
   #{&}__img {
     object-fit: contain;
-    height: 10rem;
-    width: fit-content;
+    height: 9rem;
+    width: 9rem;
     overflow: hidden;
     border-radius: 5px;
   }
 }
 
 .recipe-details {
-    width: 50%;
-    flex-direction: column;
-    text-align: center;
+  width: 50%;
+  flex-direction: column;
+  text-align: center;
 
-    @include m-flex-center;
+  @include m-flex-center;
 
-    #{&}__name {
-        padding-bottom: 0.5rem;
-    }
+  #{&}__name {
+    padding-bottom: 0.5rem;
+  }
 }
 </style>
---
+++ client/src/components/SearchPageComponents/RecipeCardSkeleton.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <div class="thumbnail-container">
+      <div class="recipe-thumbnail skeleton"></div>
+    </div>
+    <div class="recipe-details">
+      <h3 class="recipe-details__name skeleton"></h3>
+      <p class="recipe-details__area skeleton"></p>
+      <p class="recipe-details__category skeleton"></p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  flex-direction: row;
+  height: 100%;
+  outline: 1px solid $color-primary;
+  border-radius: 5px;
+  background-color: $color-surface;
+
+  @include m-flex-center;
+}
+
+.thumbnail-container {
+  width: 50%;
+  @include m-flex-center;
+}
+
+.recipe-thumbnail {
+  height: 8rem;
+  width: 8rem;
+}
+
+.recipe-details {
+  width: 50%;
+  flex-direction: column;
+  text-align: center;
+
+  @include m-flex-center;
+
+  #{&}__name {
+    height: 1.5rem;
+    width: 7rem;
+    margin-bottom: 0.5rem;
+  }
+
+  #{&}__area,
+  #{&}__category {
+    height: 1rem;
+    width: 5rem;
+    margin-bottom: 0.3rem;
+  }
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  50% {
+    transform: scale(1.08);
+    opacity: 0.85;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+.skeleton {
+  background-color: #bab9b9;
+  border-radius: 6px;
+  animation: pulse 1.5s ease-in-out infinite;
+  transform-origin: center;
+}
+</style>
