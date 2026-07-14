d4330ffa9fbca91ec04df8845c3bc77cfbc955f3
rbantug <ronbantug22@gmail.com>
2026-07-14 11:17:14 +0000

    feat: made this component more generic. There is still no styling as of this moment

--- client/src/components/SearchPageComponents/DropDown.vue
+++ client/src/components/SearchPageComponents/DropDown.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-const searchOption = defineModel<string>('search-option', { required: true })
+import { watch, type PropType } from 'vue';
+
+const props = defineProps({
+    dataList: {
+        required: true,
+        type: Array as PropType<string[]>
+    },
+    option: {
+        required: true,
+        type: Boolean,
+        default: false
+    }
+
+})
+const selectedOption = defineModel<string>('selected-option', { required: true })
+
+watch(() => props.dataList, () => {
+    if (!props.option) {
+        selectedOption.value = 'All'
+    }
+    return
+}, { immediate: true })
 </script>
 
 <template>
     <div>
-        <select v-model="searchOption">
-            <option value="keyword">Keyword Search only</option>
-            <option value="hybrid">Keyword + Semantic Search</option>
+        <select v-model="selectedOption" :disabled="props.dataList.length === 0" :class="{ disabed: props.dataList.length === 0 }">
+            <option v-if="!props.option" value="All">All</option>
+            <option v-for="data in props.dataList" :key="data" :value="data">{{ data }}</option>
         </select>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.disabled {
+    cursor: not-allowed;
+}
+</style>
