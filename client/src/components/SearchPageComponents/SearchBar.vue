fe00fc7d8a2e745195a26b69adb5bd8b9fc5faea
rbantug <ronbantug22@gmail.com>
2026-07-16 08:26:55 +0000

    feat: any opened drop down list will close if you click on the search bar

--- client/src/components/SearchPageComponents/SearchBar.vue
+++ client/src/components/SearchPageComponents/SearchBar.vue
@@ -2,22 +2,30 @@
 const model = defineModel<string>('search' ,{
   required: true,
 })
+
+const emits = defineEmits(['inputClicked'])
+
+function runEmit() {
+  emits('inputClicked')
+}
 </script>
 
 <template>
   <div>
-    <input type="text" v-model="model" placeholder="Search recipes..." />
+    <input type="text" v-model="model" placeholder="Search recipes..." @click="runEmit" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 input {
-  width: 100%;
+  width: 20rem;
+  height: 4rem;
   padding: 1rem;
   border: none;
   outline: none;
   border-radius: 10px;
   box-shadow: $shadow-search;
+  font-size: 1.5rem;
 
   &:focus {
     outline: 2px solid $color-primary;
