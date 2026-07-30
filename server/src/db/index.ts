4cd8c1f549ed76db759ce73f204ed85e1f5a4f72
rbantug <ronbantug22@gmail.com>
2026-07-30 08:05:19 +0000

    feat: changed journal mode from "rollback" to "write-ahead logging (WAL)"

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,4 +3,7 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 
 const sqlite = new Database('sqlite.db')
 
+// switch SQLite write method from the default rollback journal to write-ahead logging (WAL)
+sqlite.pragma("journal_mode = WAL");
+
 export const db = drizzle(sqlite)
