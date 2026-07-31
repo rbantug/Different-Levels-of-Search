0efe2a8fab6357f7fdcb49dd63d7b6a2f2b1c61e
rbantug <ronbantug22@gmail.com>
2026-07-31 08:18:48 +0000

    feat: added suggestion API and its type

--- client/src/api/search.ts
+++ client/src/api/search.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
-import type { Recipe } from '@/types/recipe';
+import type { Recipe } from '@/types/recipe'
 
 export interface AxiosResponse {
-    count: number;
-    data: Recipe[];
-    status: string;
-    query: string;
+  count: number
+  data: Recipe[]
+  status: string
+  query: string
 }
 
 const api = axios.create({
   baseURL: 'http://localhost:3000/api/search',
 })
 
-export async function keywordSearch(query: string):Promise<AxiosResponse> {
+export async function keywordSearch(query: string): Promise<AxiosResponse> {
   const { data } = await api.get('/', {
     params: {
       q: query,
@@ -22,8 +22,18 @@ export async function keywordSearch(query: string):Promise<AxiosResponse> {
   return data
 }
 
-export async function hybridSearch(query: string):Promise<AxiosResponse> {
+export async function hybridSearch(query: string): Promise<AxiosResponse> {
   const { data } = await api.get('/hybrid', {
+    params: {
+      q: query,
+    },
+  })
+
+  return data
+}
+
+export async function suggestion(query: string): Promise<AxiosResponse> {
+  const { data } = await api.get('/suggestion', {
     params: {
       q: query,
     },
