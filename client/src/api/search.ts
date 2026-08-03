3dfde69962b7e78b561b5e04e4e8fa0fa61daef4
rbantug <ronbantug22@gmail.com>
2026-08-03 13:13:27 +0000

    feat: - updated interface AxiosResponse - added a timeout for all axios request

--- client/src/api/search.ts
+++ client/src/api/search.ts
@@ -1,18 +1,21 @@
 import axios from 'axios'
 import type { Recipe } from '@/types/recipe'
+import type { Suggestion } from '@/types/suggestions'
 
-export interface AxiosResponse {
+export interface AxiosResponse<T> {
   count: number
-  data: Recipe[]
+  data: T[]
   status: string
   query: string
 }
 
 const api = axios.create({
   baseURL: 'http://localhost:3000/api/search',
+  timeout: 10000,
+  
 })
 
-export async function keywordSearch(query: string): Promise<AxiosResponse> {
+export async function keywordSearch(query: string): Promise<AxiosResponse<Recipe>> {
   const { data } = await api.get('/', {
     params: {
       q: query,
@@ -22,7 +25,7 @@ export async function keywordSearch(query: string): Promise<AxiosResponse> {
   return data
 }
 
-export async function hybridSearch(query: string): Promise<AxiosResponse> {
+export async function hybridSearch(query: string): Promise<AxiosResponse<Recipe>> {
   const { data } = await api.get('/hybrid', {
     params: {
       q: query,
@@ -32,7 +35,7 @@ export async function hybridSearch(query: string): Promise<AxiosResponse> {
   return data
 }
 
-export async function suggestion(query: string): Promise<AxiosResponse> {
+export async function suggestion(query: string): Promise<AxiosResponse<Suggestion>> {
   const { data } = await api.get('/suggestion', {
     params: {
       q: query,
