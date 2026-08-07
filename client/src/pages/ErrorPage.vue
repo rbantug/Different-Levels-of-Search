027c43950a5c510c5db5067bd5f80dc084f3329c
rbantug <ronbantug22@gmail.com>
2026-08-07 05:42:49 +0000

    feat: error message is now based on the http response status code

--- client/src/pages/ErrorPage.vue
+++ client/src/pages/ErrorPage.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute()
+
+const errorMsg = computed(() => {
+    if (route.params.anything === '404') {
+        return 'Recipe not found'
+    } else {
+        return 'Unexpected Error'
+    }
+})
+</script>
 
 <template>
-    <div>Error page. Go away.</div>
+    <div>{{ errorMsg }}</div>
     <router-link to="/">Back to home</router-link>
 </template>
 
