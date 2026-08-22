e9a6635c6372c82c6911284dcb642977f0217caf
rbantug <ronbantug22@gmail.com>
2026-08-22 04:16:42 +0000

    build: drizzle database schema generation and migration done

---
+++ server-ca/drizzle/0000_nervous_callisto.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `recipes` (
+	`id` text PRIMARY KEY NOT NULL,
+	`recipe_name` text NOT NULL,
+	`category` text NOT NULL,
+	`area` text NOT NULL,
+	`slug` text NOT NULL,
+	`instructions` text NOT NULL,
+	`recipeThumbnail` text,
+	`ingredients` text NOT NULL,
+	`keywords` text NOT NULL,
+	`embedding` text NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `recipes_slug_unique` ON `recipes` (`slug`);--> statement-breakpoint
+CREATE TABLE `recipe_keywords` (
+	`recipe_id` text NOT NULL,
+	`keyword` text NOT NULL,
+	PRIMARY KEY(`recipe_id`, `keyword`),
+	FOREIGN KEY (`recipe_id`) REFERENCES `recipes`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE INDEX `recipe_keywords_keyword_idx` ON `recipe_keywords` (`keyword`);
