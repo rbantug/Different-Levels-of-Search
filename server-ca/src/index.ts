4354a4a34067a57062d2da1f270e4336a80741c9
rbantug <ronbantug22@gmail.com>
2026-08-22 03:47:00 +0000

    feat: added routes and error handling as express middlewares

--- server-ca/src/index.ts
+++ server-ca/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 
+import recipeRoutes from "./routes/recipeRoutes.js"
+import searchRoutes from "./routes/searchRoutes.js"
+import errorHandler from './middleware/errorHandler.js'
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (_, res) => {
-  res.json({
-    status: "ok",
-  });
-});
+app.use("/api/recipes", recipeRoutes);
+app.use("/api/search", searchRoutes);
+
+app.use(errorHandler)
 
 const PORT = 3000;
 
