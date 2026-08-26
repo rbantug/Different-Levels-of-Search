20021144ccfd733b542f40e82bf9af4719075c22
rbantug <ronbantug22@gmail.com>
2026-08-26 03:43:22 +0000

    refactor: hardcoded the path of the new sqlite DB

--- server-ca/src/database/index.ts
+++ server-ca/src/database/index.ts
@@ -1,13 +1,7 @@
 import createDB from "./client.js";
 import makeRecipeDB from "./recipeDB.js";
 
-const databasePath = process.env.DATABASE_PATH;
-
-if (!databasePath) {
-  throw new Error("DATABASE_PATH is required");
-}
-
-const db = createDB(databasePath);
+const db = createDB("sqlite.db");
 const recipeDB = makeRecipeDB({ db })
 
 export {
