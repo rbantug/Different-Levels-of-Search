c2de0ee035551953cc1147892fa2e0b60847b674
rbantug <ronbantug22@gmail.com>
2026-09-01 04:21:57 +0000

    fix: no results from meilisearch will immediately return an empty array

--- server-ca/src/use-cases/searchRecipesByKeyword.ts
+++ server-ca/src/use-cases/searchRecipesByKeyword.ts
@@ -1,19 +1,22 @@
 import type makeRecipeIndex from "../services/meilisearch/recipeIndex.js";
 import type makeRecipeDB from "../database/recipeDB.js";
-import { recipeDB } from "../database/index.js";
 
 interface Dependencies {
   recipeIndex: {
-    searchRecipe: ReturnType<typeof makeRecipeIndex>['searchRecipe'];
-    recipeDB: {
-      findRecipesById: ReturnType<typeof makeRecipeDB>['findRecipesById']
-    }
+    searchRecipe: ReturnType<typeof makeRecipeIndex>["searchRecipe"];
+  };
+  recipeDB: {
+    findRecipesById: ReturnType<typeof makeRecipeDB>["findRecipesById"];
   };
 }
 
-export default function makeSearchRecipesByKeyword({ recipeIndex }: Dependencies) {
+export default function makeSearchRecipesByKeyword({ recipeIndex, recipeDB }: Dependencies) {
   return async function searchRecipesByKeyword({ query, limit }: { query: string, limit?: number | undefined }) {
     const result = await recipeIndex.searchRecipe({ query, limit });
+
+    if (result.hits.length === 0) {
+      return []
+    }
 
     // fetch full recipe from DB using search result
     const ids = result.hits.map(hit => hit.id)
