479bf328db25c764d51cf5413911651da8f169fd
rbantug <ronbantug22@gmail.com>
2026-09-12 03:27:48 +0000

    refactor: created makeSearchRoutes() to make it easier to test the routes and controllers

--- server-ca/src/routes/searchRoutes.ts
+++ server-ca/src/routes/searchRoutes.ts
@@ -2,62 +2,72 @@ import { Router } from "express";
 
 import expressCallback from "../services/express/expressCallback.js";
 
-import {
-  getSearchRecipes,
-  getHybridRecipe,
-  getKeywordSuggestions,
-} from "../controllers/index.js";
+import type makeGetHybridRecipe from "../controllers/getHybridRecipes.js";
+import type makeGetKeywordSuggestions from "../controllers/getKeywordSuggestions.js";
+import type makeGetSearchRecipe from "../controllers/getSearchRecipes.js";
 
 import { getRequiredQuery } from "../services/express/getQueryOrParam.js";
 
-const router = Router();
+interface Dependencies {
+  getSearchRecipes: ReturnType<typeof makeGetSearchRecipe>;
+  getHybridRecipe: ReturnType<typeof makeGetHybridRecipe>;
+  getKeywordSuggestions: ReturnType<typeof makeGetKeywordSuggestions>;
+}
 
-router.route("/").get(
-  expressCallback({
-    controller: getSearchRecipes,
-    mapRequest: (req) => ({
-      query: getRequiredQuery({
-        query: req.query,
-        name: "q",
-      }),
-      limit: parseInt(
-        getRequiredQuery({
+export default function makeSearchRoutes({
+  getSearchRecipes,
+  getHybridRecipe,
+  getKeywordSuggestions,
+}: Dependencies) {
+  const router = Router();
+
+  router.route("/").get(
+    expressCallback({
+      controller: getSearchRecipes,
+      mapRequest: (req) => ({
+        query: getRequiredQuery({
           query: req.query,
-          name: "limit",
+          name: "q",
         }),
-      ),
-    }),
-  }),
-);
-
-router.route("/hybrid").get(
-  expressCallback({
-    controller: getHybridRecipe,
-    mapRequest: (req) => ({
-      query: getRequiredQuery({
-        query: req.query,
-        name: "q",
+        limit: parseInt(
+          getRequiredQuery({
+            query: req.query,
+            name: "limit",
+          }),
+        ),
       }),
-      limit: parseInt(
-        getRequiredQuery({
+    }),
+  );
+
+  router.route("/hybrid").get(
+    expressCallback({
+      controller: getHybridRecipe,
+      mapRequest: (req) => ({
+        query: getRequiredQuery({
           query: req.query,
-          name: "limit",
+          name: "q",
         }),
-      ),
-    }),
-  }),
-);
-
-router.route("/suggestion").get(
-  expressCallback({
-    controller: getKeywordSuggestions,
-    mapRequest: (req) => ({
-      query: getRequiredQuery({
-        query: req.query,
-        name: "q",
+        limit: parseInt(
+          getRequiredQuery({
+            query: req.query,
+            name: "limit",
+          }),
+        ),
       }),
     }),
-  }),
-);
+  );
 
-export default router;
+  router.route("/suggestion").get(
+    expressCallback({
+      controller: getKeywordSuggestions,
+      mapRequest: (req) => ({
+        query: getRequiredQuery({
+          query: req.query,
+          name: "q",
+        }),
+      }),
+    }),
+  );
+
+  return router;
+}
