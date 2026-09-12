a0a7e3c34f8ff19f621a204cb38d438ff15ad5f2
rbantug <ronbantug22@gmail.com>
2026-09-12 03:29:56 +0000

    fix: added config to sqlite to allow foreign key on every database startup

--- server-ca/src/database/client.ts
+++ server-ca/src/database/client.ts
@@ -3,6 +3,8 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 
 export default function createDB(dbPath: string) {
   const sqlite = new Database(dbPath);
+  // this will enable foreign keys constraint on each database startup
+  sqlite.pragma("foreign_keys = ON")
   return drizzle(sqlite);
 }
 
