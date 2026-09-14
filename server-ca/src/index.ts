f087d601f0694233a3fb82f307f0edf67c8c4905
rbantug <ronbantug22@gmail.com>
2026-09-14 09:15:58 +0000

    feat: meiliRecipeIndex.setupRecipeIndex() and meiliKeywordIndex.setupKeywordIndex() will now run before the recipe import script

--- server-ca/src/index.ts
+++ server-ca/src/index.ts
@@ -3,6 +3,7 @@ import createApp from "./app.js";
 import runImport from "../scripts/importRecipes.js";
 import { db } from "./database/index.js";
 import { recipes } from "./database/schemas/recipe.js";
+import { meiliRecipeIndex, meiliKeywordIndex } from "./services/meilisearch/index.js";
 
 import {
   deleteSingleRecipe,
@@ -21,6 +22,9 @@ async function start() {
   const checkDB = db.select().from(recipes).limit(1).get();
 
   if (!checkDB) {
+    await meiliRecipeIndex.setupRecipeIndex();
+    await meiliKeywordIndex.setupKeywordIndex();
+    
     await runImport();
   }
 
